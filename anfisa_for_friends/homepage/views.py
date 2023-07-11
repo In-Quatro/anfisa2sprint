a16d6c891459f49102da47cbf642867b7d9325cc
Алексей Шубенков <shubenkov-93@yandex.ru>
2023-07-11 22:48:55 +0300

    finish project

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -4,15 +4,16 @@ from django.shortcuts import render
 from ice_cream.models import IceCream
 
 def index(request):
-    template_name = 'homepage/index.html'
-    # Запрос:
-    ice_cream_list = IceCream.objects.values(
-        'id', 'title', 'description'
-        ).filter(is_published=True)
-    
-    # Полученный из БД QuerySet передаём в словарь контекста:
+    template = 'homepage/index.html'
+    ice_cream_list = (IceCream.objects.values(
+        'id', 'title', 'price', 'description'
+        ).filter(is_on_main=True,
+                is_published=True,
+                category__is_published=True
+        )
+        .order_by('title')
+    )
     context = {
         'ice_cream_list': ice_cream_list,
     }
-    # Словарь контекста передаём в шаблон, рендерим HTML-страницу:
-    return render(request, template_name, context)
+    return render(request, template, context)
